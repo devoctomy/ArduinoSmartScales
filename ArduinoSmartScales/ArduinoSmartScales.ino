d43916842934acd11275b98e063d6eb9c6ae9fb6
Nick Pateman <nick@devoctomy.com>
2020-09-05 11:32:30 +0100

    Initial commit

---
+++ ArduinoSmartScales/ArduinoSmartScales.ino
@@ -0,0 +1,143 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <LiquidMenu.h>
+#include "HX711.h"
+#include "ButtonManager.h"
+
+#define DOUT A2
+#define CLK  A3
+#define AVERAGESAMPLECOUNT 7
+#define BUZZER 8
+#define MENU_BUTTON 0
+#define SELECT_BUTTON 1
+#define LEFT_BUTTON 2
+#define RIGHT_BUTTON 3
+#define SMARTSCALES_VERSION "1.0"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+HX711 scale;
+
+int encoderButtonPin = 2;
+int encoderClockwisePin = 3;
+int encoderAntiClockwisePin = 4;
+int homeButtonPin = 5;
+
+float calibration_factor = 431;
+long baseline = 0;
+unsigned short analogReading = 0;
+bool menuRequiresUpdate = true;
+
+LiquidLine welcome_line1(1, 0, "SmartScales ", SMARTSCALES_VERSION);
+LiquidLine welcome_line2(1, 1, "");
+LiquidScreen welcome_screen(welcome_line1, welcome_line2);
+
+LiquidLine analogReading_line(0, 0, "Analog: ", analogReading);
+LiquidScreen secondary_screen(analogReading_line);
+LiquidMenu menu(lcd);
+
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  menu.init();
+  menu.add_screen(welcome_screen);
+  menu.add_screen(secondary_screen);
+  
+  lcd.print("Please wait...");
+  
+  Serial.begin(9600);
+
+  Serial.println("Configuring buttons");
+  AddManagedButton({
+    "Home",
+    homeButtonPin,
+    true,
+    (ButtonCallbackDelegate)ManagedButtonCallback
+  });
+
+  Serial.println("Configuring encoder");
+  AddManagedEncoder({
+    "Encoder",
+    encoderClockwisePin,
+    encoderAntiClockwisePin,
+    encoderButtonPin,
+    true,
+    (ButtonCallbackDelegate)ManagedButtonCallback,
+    (EncoderCallbackDelegate)ManagedEncoderCallback
+  });
+
+  Serial.println("Initialising");
+  scale.begin(DOUT, CLK);
+  scale.set_scale();
+
+  Serial.println("Resetting");
+  scale.tare();
+
+  Serial.println("Getting baseline");
+  baseline = scale.read_average();
+  Serial.print("Baseline: ");
+  Serial.println(baseline);
+
+  lcd.clear();
+}
+
+float GetAveragedSamples(int sampleCount)
+{
+  float sample = scale.get_units(sampleCount);
+  if(sample < 0) sample = 0;
+  return sample;
+}
+
+void loop()
+{
+  /*
+  scale.set_scale(calibration_factor);
+
+  Serial.print("Reading: ");
+
+  float averageSample = GetAveragedSamples(AVERAGESAMPLECOUNT);
+
+  lcd.clear();
+  lcd.print(averageSample);
+  lcd.print("g");
+  
+  Serial.print(averageSample);
+  Serial.print(" grams"); 
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+  */
+
+  CheckManagedButtons();
+  
+  if(menuRequiresUpdate)
+  {
+    menu.update();
+    menuRequiresUpdate = false;
+  }
+}
+
+void ManagedButtonCallback(String key, ButtonState buttonState)
+{
+  if(key == "Home" && buttonState == ButtonState::ButtonDepressed)
+  {
+      menu.previous_screen();
+  }
+  else if(key == "Encoder.Button" && buttonState == ButtonState::ButtonDepressed)
+  {
+      menu.next_screen();
+  }
+}
+
+void ManagedEncoderCallback(String key, EncoderState encoderState)
+{
+  if(encoderState == EncoderState::EncoderClockwise)
+  {
+    analogReading += 1;
+  }
+  else
+  {
+    analogReading -= 1;
+  }
+  menuRequiresUpdate = true;
+}
---
+++ ArduinoSmartScales/ButtonManager.h
@@ -0,0 +1,142 @@
+enum ButtonState
+{
+  ButtonDepressed,
+  ButtonPressed
+};
+
+enum EncoderState
+{
+  EncoderClockwise,
+  EncoderAntiClockwise
+};
+
+typedef void (*ButtonCallbackDelegate) (String key, ButtonState buttonState);
+typedef void (*EncoderCallbackDelegate) (String key, EncoderState encoderState);
+
+struct EncoderDef
+{
+  String Key;
+  int ClockwiseDigitalPin;
+  int AntiClockwiseDigitalPin;
+  int ButtonDigitalPin;
+  bool ButtonPullUp;
+  ButtonCallbackDelegate ButtonCallback;
+  EncoderCallbackDelegate EncoderCallback;  
+  int ClockwiseButtonIndex;
+  int AntiClockwiseButtonIndex;
+  int ButtonIndex;
+  int LastVal;
+};
+
+struct ButtonDef
+{
+  String Key;
+  int DigitalPin;
+  bool PullUp;
+  ButtonCallbackDelegate ButtonCallback;
+  EncoderDef *EncoderParent;
+  int LastVal;
+};
+
+ButtonDef managedButtonDefs[12];
+EncoderDef managedEncoderDefs[4];
+int managedButtonCount = 0;
+int managedEncoderCount = 0;
+char data[100];
+
+int CheckButton(ButtonDef *buttonDef)
+{
+   int buttonVal = digitalRead(buttonDef->DigitalPin);     //Read button value  
+   int lastVal = buttonDef->LastVal;                       //Get last value
+   if(buttonVal != lastVal)                               //Compare new value against last value
+   {
+    buttonDef->LastVal = buttonVal;                        //Store current value as last value
+    if(buttonDef->ButtonCallback != NULL)
+    {
+      buttonDef->ButtonCallback(buttonDef->Key, lastVal == 0 ?
+        ButtonState::ButtonDepressed :
+        ButtonState::ButtonPressed);
+    }
+    return buttonVal;
+   }
+}
+
+void CheckEncoder(EncoderDef *encoderDef)
+{
+  ButtonDef clockwiseButton = managedButtonDefs[encoderDef->ClockwiseButtonIndex];
+  ButtonDef antiClockwiseButton = managedButtonDefs[encoderDef->AntiClockwiseButtonIndex];
+  int clockwise = CheckButton(&clockwiseButton);
+  if(encoderDef->LastVal == LOW && clockwise == HIGH)
+  {
+    int antiClockwise = CheckButton(&antiClockwiseButton);
+
+    encoderDef->EncoderCallback(encoderDef->Key, antiClockwise == HIGH ?
+      EncoderState::EncoderClockwise :
+      EncoderState::EncoderAntiClockwise);
+  }
+  encoderDef->LastVal = clockwise;
+}
+
+int AddManagedButton(ButtonDef buttonDef)
+{
+  if(buttonDef.PullUp)
+  {
+    pinMode (buttonDef.DigitalPin, INPUT_PULLUP);
+    buttonDef.LastVal = HIGH;
+  }
+  else
+  {
+    pinMode (buttonDef.DigitalPin, INPUT);
+  }
+  managedButtonDefs[managedButtonCount] = buttonDef;
+  managedButtonCount += 1;
+  return managedButtonCount-1;
+}
+
+void AddManagedEncoder(EncoderDef encoderDef)
+{
+  ButtonDef clocklwiseButton = {
+    encoderDef.Key + ".Clockwise",
+    encoderDef.ClockwiseDigitalPin,
+    false,
+    NULL,
+    &encoderDef
+  };
+  ButtonDef antiClocklwiseButton = {
+    encoderDef.Key + ".AntiClockwise",
+    encoderDef.AntiClockwiseDigitalPin,
+    false,
+    NULL,
+    &encoderDef
+  };
+  ButtonDef button = {
+    encoderDef.Key + ".Button",
+    encoderDef.ButtonDigitalPin,
+    true,
+    encoderDef.ButtonCallback
+  };
+
+  encoderDef.ClockwiseButtonIndex = AddManagedButton(clocklwiseButton);
+  encoderDef.AntiClockwiseButtonIndex = AddManagedButton(antiClocklwiseButton);
+  encoderDef.ButtonIndex = AddManagedButton(button);
+
+  managedEncoderDefs[managedEncoderCount] = encoderDef;
+  managedEncoderCount += 1;
+}
+
+void CheckManagedButtons()
+{
+  int i = 0;
+  for(i = 0; i < managedButtonCount; i++)
+  {
+    if(managedButtonDefs[i].EncoderParent == NULL)
+    {
+      CheckButton(&managedButtonDefs[i]);
+    }
+  }
+
+  for(i = 0; i < managedEncoderCount; i++)
+  {
+    CheckEncoder(&managedEncoderDefs[i]);
+  }
+}
